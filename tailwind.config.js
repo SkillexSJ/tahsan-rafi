b14df1740468e5a8aed652f53220c9513a1ed634
SKillexSJ <sajid7271.sk@gmail.com>
2025-07-01 12:21:37 +0600

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        light1: "#EAEAEA",
+        light2: "#DBDBDB",
+        light3: "#F2F2F2",
+        graygold: "#ADA996",
+      },
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(to right, #EAEAEA, #DBDBDB, #F2F2F2, #ADA996)",
+      },
+    },
+  },
+};
